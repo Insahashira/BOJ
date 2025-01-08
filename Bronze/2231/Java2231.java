1c34ea3ffa87c6e1ff1b7596205fedf90c4836bd
minjun <2004mjlee@gmail.com>
2025-01-08 23:25:02 +0900

    2231 분해합

---
+++ Bronze/2231/Java2231.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Java2231 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+        int ori = 1;
+        int temp;
+
+        while(true){
+            ori++;
+            temp = ori + converter(""+ori);
+            if (input == temp){
+                System.out.println(ori);
+                break;
+            }
+            if (temp > 1000000){
+                System.out.println(0);
+                break;
+            }
+        }
+    }
+
+    static int converter(String input){
+        int result = 0;
+        for(int i = 0; i < input.length(); i++){
+            result += (int)input.charAt(i) - 48;
+        }
+        return result;
+    }
+}
