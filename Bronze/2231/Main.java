6c406b4babf2436021cae2d14d210db47971b82d
minjun <2004mjlee@gmail.com>
2025-01-27 17:01:38 +0900

    2231 분해합

--- Bronze/2231/Main.java
+++ Bronze/2231/Main.java
@@ -1,8 +1,10 @@
+package Java2231;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Java2231 {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int input = Integer.parseInt(br.readLine());
