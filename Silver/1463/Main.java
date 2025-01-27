8ca799444c3ebf793b24581931cf42e1818b126f
minjun <2004mjlee@gmail.com>
2025-01-27 17:08:37 +0900

    1463 1로 만들기

--- Silver/1463/Main.java
+++ Silver/1463/Main.java
@@ -1,8 +1,10 @@
+package Silver1463;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Java1463 {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
