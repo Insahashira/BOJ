e6a5a1ac80e666671273abc990cf47a7428b3201
minjun <2004mjlee@gmail.com>
2025-02-08 02:31:26 +0900

    10815 숫자 카드

---
+++ Silver/10815/Main.java
@@ -0,0 +1,28 @@
+package silver10815;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+
+        String[] num = br.readLine().split(" ");
+
+        HashSet<String> ts = new HashSet<>(Arrays.asList(num));
+
+        count = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        num = br.readLine().split(" ");
+        for(String n : num){
+            sb = ts.contains(n) ? sb.append("1") : sb.append("0");
+            sb.append(" ");
+        }
+        System.out.println(sb);
+    }
+}
