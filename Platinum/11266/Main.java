d58413c674c659a79471136bf16398ed04070f30
Minjun Lee <minjun.lee.1@stonybrook.edu>
2026-01-31 01:11:30 -0500

    11266 on going

--- Platinum/11266/Main.java
+++ Platinum/11266/Main.java
@@ -18,7 +18,7 @@ public class Main {
         visit_order = new int[vertice];
 
         e = (ArrayList<Integer>[]) new ArrayList[vertice];
-        for(int i = 0; i < edge; i++){
+        for(int i = 0; i < vertice; i++){
             e[i] = new ArrayList<>();
         }
 
@@ -31,7 +31,7 @@ public class Main {
             e[from].add(to);
             e[to].add(from);
         }
-        DFS(0);
+        for(int i = 0; i < vertice; i++) if(visit_order[i] == 0) DFS(i, i);
         node_dump();
     }
 
@@ -39,26 +39,32 @@ public class Main {
         if(!cut_node.contains(index)) cut_node.add(index);
     }
 
-    public static int DFS(int idx){
-        if(visit_order[idx] != 0){
-            return idx;
-        }else{
+    public static int DFS(int par, int idx){
+        int cur_order = visit_order[idx];
+        if(cur_order == 0){
             visit_order[idx] = visit_counter++;
+            cur_order = visit_order[idx];
+
+            if(e[idx].isEmpty() ||(e[idx].size() == 1 && par != idx)) return cur_order;
+
+            int lowest = 0x7fffffff; 
             for(int i = 0; i < e[idx].size(); i++){
                 int candidate = e[idx].get(i);
-                int result = DFS(candidate);
+                if(i != par){
+                    int result = DFS(idx, candidate);
+                    lowest = Math.min(lowest, result);
+                }
             }
-        }
-        
-        return 0;
-
+            if(lowest >= cur_order) add_cutting_node(idx);
+            return lowest;
+        }else return cur_order;
     }
 
     public static void node_dump(){
         System.out.println(cut_node.size());
         Collections.sort(cut_node);
         for(int i = 0; i < cut_node.size(); i++){
-            System.out.print(cut_node.get(i)+ 1 + " ");
+            System.out.print(cut_node.get(i) + 1 + " ");
         }
     }
 }
