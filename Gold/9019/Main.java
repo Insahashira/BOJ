6586343b9090e690706ad95c1aba2adcbe428a29
Insahashira <2k5mjlee@gamil.com>
2026-03-14 22:55:57 -0400

    9019 DSLR

--- Gold/9019/Main.java
+++ Gold/9019/Main.java
@@ -1,93 +1,93 @@
-package Gold9019;
+    // package Gold9019;
 
-import java.io.*;
-import java.util.*;
+    import java.io.*;
+    import java.util.*;
 
-public class Main {
-   private static class SN{ //StringNode
-       char ch;
-       SN next;
+    public class Main {
+    private static class SN{ //StringNode
+        char ch;
+        SN next;
 
-       public SN(char ch, SN next){
-           this.ch = ch;
-           this.next = next;
-       }
-   }
+        public SN(char ch, SN next){
+            this.ch = ch;
+            this.next = next;
+        }
+    }
 
-   private static class Node{
-       int val;
-       SN op;
-       Boolean isL;
+    private static class Node{
+        int val;
+        SN op;
 
-       public Node(int val, SN op, Boolean isL){
-           this.val = val;
-           this.op = op;
-           this.isL = isL;
-       }
-   }
+        public Node(int val, SN op){
+            this.val = val;
+            this.op = op;
+        }
+    }
 
-   public static void main(String[] args) throws IOException {
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       StringTokenizer st = new StringTokenizer(br.readLine());
-       Queue<Node> q;
-       int count = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Queue<Node> q;
+        int count = Integer.parseInt(st.nextToken());
 
-       for (int i = 0; i < count; i++) {
-           boolean[] visited = new boolean[10000];
-           q = new LinkedList<>();
+        for (int i = 0; i < count; i++) {
+            boolean[] visited = new boolean[10000];
+            q = new LinkedList<>();
 
-           st = new StringTokenizer(br.readLine());
-           int from = Integer.parseInt(st.nextToken());
-           int to = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to   = Integer.parseInt(st.nextToken());
 
-           q.add(new Node(to, new SN((char)0, null), null));
+            q.add(new Node(to, new SN((char)0, null)));
+            while(!q.isEmpty()) {
+                Node n = q.poll();
 
-           while(!q.isEmpty()) {
-               Node n = q.poll();
-               int temp = n.val;
-               visited[temp] = true;
+                int temp = n.val;
+                if(visited[temp] == true) continue;
+                visited[temp] = true;
 
-               SN source = n.op;
-               Boolean isL = n.isL;
+                SN source = n.op;
 
-               if(temp == from){
-                   StringBuilder sb = new StringBuilder();
+                if(temp == from){
+                    fin(source);
+                    break;
+                }
 
-                   while(source.ch != 0){
-                       sb.append(source.ch);
-                       source = source.next;
-                   }
+                // D
+                if(temp % 2 == 0){
+                    if(!visited[temp/2])        q.add(new Node(temp/2, new SN('D', source)));
+                    if(!visited[temp/2 + 5000]) q.add(new Node(temp/2 + 5000, new SN('D', source)));
+                }
 
-                   sb.append("\n");
-                   System.out.print(sb);
-                   break;
-               }
+                // S
+                if(temp == 9999 && !visited[0]) q.add(new Node(0, new SN('S', source)));
+                else if(!visited[temp + 1])     q.add(new Node(temp + 1, new SN('S', source)));
+                
+                int temp2;
+                // L
+                temp2 = temp % 10;
+                temp2 = (temp2 * 1000) + (temp / 10);
+                
+                if(!visited[temp2]) q.add(new Node(temp2, new SN('L', source)));
+                // R
+                temp2 = temp / 1000;
+                temp2 = temp2 + (temp * 10) % 10000;
 
-               if(temp % 2 == 0){
-                   if(!visited[temp/2])                         q.add(new Node(temp/2, new SN('D', source), isL));
-                   if(temp < 5000 && !visited[(temp + 5000)/2]) q.add(new Node((temp + 5000)/2, new SN('D', source), isL));
-               }
+                if(!visited[temp2]) q.add(new Node(temp2, new SN('R', source)));
+                
+            }
+        }
+    }
 
-               if(temp == 9999 && !visited[0]) q.add(new Node(0, new SN('S', source), isL));
-               else if(!visited[temp + 1])     q.add(new Node(temp + 1, new SN('S', source), isL));
+    public static void fin(SN source){
+        StringBuilder sb = new StringBuilder();
 
-               int temp2;
-               if(isL == null || isL) {
-                   temp2 = temp % 10;
-                   temp2 = (temp2 * 1000) + (temp / 10);
+        while(source.ch != 0){
+            sb.append(source.ch);
+            source = source.next;
+        }
 
-//                    if(!visited[temp2])
-                   q.add(new Node(temp2, new SN('L', source), true));
-               }
-
-               if(isL == null || !isL) {
-                   temp2 = temp / 1000;
-                   temp2 = temp2 + (temp * 10) % 10000;
-
-//                    if(!visited[temp2])
-                   q.add(new Node(temp2, new SN('R', source), false));
-               }
-           }
-       }
-   }
-}
+        sb.append("\n");
+        System.out.print(sb);
+    }
+    }
